fe3f3e375dd0f2f6e8d2f3979fbc9225fbd774da
DwinandaBagoes <81345045+DwinandaBagoes@users.noreply.github.com>
2021-12-14 10:42:42 +0700

    add music player helper

--- script.js
+++ script.js
@@ -46,3 +46,54 @@ const deviceInfo = (function(){
   },
  }; 
 })();
+
+/**
+ * Music player helper 
+ */
+const musicHelper = (function(){
+ let wrap   = document.querySelector( '#player' ); 
+ let button = wrap ? wrap.querySelector( 'button' ) : null; 
+ let audio  = new Audio( 'http://ice1.somafm.com/u80s-256-mp3' ); 
+ let step   = 0.01;
+ let active = false; 
+ let sto    = null; 
+ 
+ let fadeIn = () => {
+  audio.volume += 0.01; 
+  if ( audio.volume >= 0.2 ) { audio.volume = 0.2; return; }
+  sto = setTimeout( fadeIn, 100 ); 
+ };
+ 
+ let fadeOut = () => {
+  audio.volume -= 0.02; 
+  if ( audio.volume <= 0.01 ) { audio.volume = 0; audio.pause(); return; }
+  sto = setTimeout( fadeOut, 100 ); 
+ };
+ 
+ let play = () => {
+  if ( sto ) clearTimeout( sto ); 
+  active = true;
+  button.textContent = 'Stop music'; 
+  audio.play(); 
+  fadeIn();
+ };
+ 
+ let stop = () => {
+  if ( sto ) clearTimeout( sto ); 
+  active = false;
+  button.textContent = 'Play music'; 
+  fadeOut();
+ };
+ 
+ button.addEventListener( 'click', e => {
+  e.stopPropagation(); 
+  e.preventDefault(); 
+  if ( active ) { stop(); } 
+  else { play(); }
+ });
+ 
+ audio.preload = 'auto'; 
+ audio.muted   = false; 
+ audio.volume  = 0;
+ return { play, stop };
+})();
